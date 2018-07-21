5fb6f8bbf6a227ae3da4e1d6f81c48a4f5cc9fa9
varsha60 <varshasawlani60@gmail.com>
2018-07-22 03:55:16 +0530

    Database handled

--- Pages/Classes/User.php
+++ Pages/Classes/User.php
@@ -37,49 +37,34 @@ include('Pages/Classes/DB.php');
     public static function createDonor($name,$email_id,$password,$amount)
 
     {
-      if(!DB::query('SELECT name from where name = :name',array(':name'=>$name)))
+
+      if(!DB::query('SELECT name from donor where name = :name',array(':name'=>$name)))
       {
         DB::query('INSERT INTO donor(name,email_id,password,amount) values(:name,:email_id,:password,:amount)',array(':name'=>$name,':email_id'=>$email_id,':password'=>password_hash($password, PASSWORD_BCRYPT),'amount'=>$amount));
-      
-        return 1;
-      }
-      else
-      {
-        return 0;
-      }
-      
+    return 1;
+    }
+    else{
+    return 0;
+     }
+     
     }
 
 
     public static function createVolunteers($name,$contact_no,$password,$email_id)
 
     {
-      if(self::nameExists($name,'volunteers'))
+
+      if(!DB::query('SELECT name from volunteers where name = :name',array(':name'=>$name)))
       {
         DB::query('INSERT INTO volunteers(name,contact_no,password,email_id) values(:name,:contact_no,:password,:email_id)',array(':name'=>$name,':contact_no'=>$contact_no,':password'=>password_hash($password, PASSWORD_BCRYPT),':email_id'=>$email_id));
-        return 1;
-      }
-      else
-      {
-        return 0;
-      }
-      
+    return 1;
     }
+    else{
+    return 0;
+     }
 
-    // public static function userLogin($username,$password)
-    // {
-    //   if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
-    //           if (password_verify($password, DB::query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'])) {
-    //                 $_SESSION['login_username']= DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))[0]['username'];
-    //                 return 'Success!';
-
-
-    //           } else {
-    //                   return 'Incorrect Password!';
-    //           }
-    //   } else {
-    //           return 'User not registered!';
-    //   }
-    // }
+      
+     
+    }
  }
 ?>
