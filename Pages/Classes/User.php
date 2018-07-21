42c983db4ba1c5cbca0d47a9e866c3a2cc1d8b22
ashwinidhande13 <ashdhande13@gmail.com>
2018-07-22 04:02:53 +0530

    Login service created

--- Pages/Classes/User.php
+++ Pages/Classes/User.php
@@ -81,5 +81,24 @@ include('Pages/Classes/DB.php');
     //           return 'User not registered!';
     //   }
     // }
+
+
+    public static function studentLogin($name,$password)
+    {
+        if(DB:query('SELECT name from student_registered WHERE name=:name',array(':name'=>$name))){
+          if (password_verify($password, DB::query('SELECT password FROM student_registred WHERE name=:name', array(':name'=>$name))[0]['password'])) {
+                             $_SESSION['login_username']= DB::query('SELECT name FROM student_registred WHERE name=:name', array(':name'=>$name))[0]['name'];
+                          return 'Success!';
+        
+        
+                       } else {
+                               return 'Incorrect Password!';
+                       }
+              } else {
+                     return 'User not registered!';
+              }
+        }
+
+    }
  }
 ?>
